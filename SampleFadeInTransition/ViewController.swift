81b559e7a8590b81ba65902eeadd7d3b6826d150
fuhi1983 <fuhi1983@gmail.com>
2015-05-02 18:01:53 +0900

    プロジェクト作成

---
+++ SampleFadeInTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleFadeInTransition
+//
+//  Created by FUJISAWAHIROYUKI on 2015/05/02.
+//  Copyright (c) 2015年 swift-studying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
